5d8ea3d16f4ea2fbb8dde402f44028cefe52bc17
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-04-05 00:13:21 -0600

    Movies problem

---
+++ can_two_movies_fill_flight.py
@@ -0,0 +1,15 @@
+  def can_two_movies_fill_flight(movie_lengths, flight_length):
+
+    # movie lengths we've seen so far
+    movie_lengths_seen = set()
+
+    for first_movie_length in movie_lengths:
+
+        matching_second_movie_length = flight_length - first_movie_length
+        if matching_second_movie_length in movie_lengths_seen:
+            return True
+
+        movie_lengths_seen.add(first_movie_length)
+
+    # we never found a match, so return False
+    return False
