a951a67bb001976bdb0e3504b0d1df08d3dbd0e8
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2018-03-11 14:56:44 -0600

    Printed good result only

--- can_two_movies_fill_flight.py
+++ can_two_movies_fill_flight.py
@@ -9,13 +9,13 @@ def can_two_movies_fill_flight(movie_lengths, flight_length):
 
         matching_second_movie_length = flight_length - first_movie_length
         if matching_second_movie_length in movie_lengths_seen:
+            final_set.add(first_movie_length)
+            final_set.add(matching_second_movie_length)
             print(final_set)
             return True
 
         movie_lengths_seen.add(first_movie_length)
         
-        final_set.add(first_movie_length)
-        final_set.add(matching_second_movie_length)
 
     # we never found a match, so return False
     return False
